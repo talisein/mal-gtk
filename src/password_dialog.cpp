d6442431f6356fb351f8f1a166d360d779a6e6fc
Andrew Potter <agpotter@gmail.com>
2013-04-22 14:00:25 -0700

    Add GPL3 header to source files

--- src/password_dialog.cpp
+++ src/password_dialog.cpp
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of mal-gtk.
+ *
+ *  mal-gtk is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  mal-gtk is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with mal-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "password_dialog.hpp"
 #include <gtkmm/stock.h>
 #include <gtkmm/label.h>
