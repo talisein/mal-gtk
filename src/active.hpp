f3732a8b156fb46d25fab544b1153e440aa8ddc4
Andrew Potter <agpotter@gmail.com>
2013-10-13 15:16:57 -0700

    Active: Add copyright header

--- src/active.hpp
+++ src/active.hpp
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of mal-gtk.
+ *
+ *  mal-gtk is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  mal-gtk is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with mal-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 #include <memory>
 #include <thread>
@@ -22,7 +39,7 @@ namespace MAL {
             queue.push(std::forward(t));
             cond.notify_one();
         };
-            
+
         T pop() {
             std::unique_lock<std::mutex> lock(m);
             while (queue.empty()) {
