1543814b26b7189301bc697728f95a6a3be521cc
Andrew Potter <agpotter@gmail.com>
2015-05-03 16:47:02 -0700

    CellRendererScore: Use new G_DECLARE_DERIVABLE_TYPE macro

--- src/gui/malgtk_cellrenderer_score.c
+++ src/gui/malgtk_cellrenderer_score.c
@@ -1,32 +1,42 @@
-#include <glib.h>
 #include "malgtk_cellrenderer_score.h"
 
+struct _MalgtkCellRendererScorePrivate
+{
+    gint           score;
+    GtkListStore *liststore;
+};
+typedef struct _MalgtkCellRendererScorePrivate MalgtkCellRendererScorePrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (MalgtkCellRendererScore, malgtk_cell_renderer_score, GTK_TYPE_CELL_RENDERER_COMBO)
+
+
 /* Some boring function declarations: GObject type system stuff */
+static void     malgtk_cell_renderer_score_init            (MalgtkCellRendererScore      *cellscore);
 
-static void     malgtk_cell_renderer_score_init       (MalgtkCellRendererScore      *cellscore);
+static void     malgtk_cell_renderer_score_class_init      (MalgtkCellRendererScoreClass *klass);
 
-static void     malgtk_cell_renderer_score_class_init (MalgtkCellRendererScoreClass *klass);
+static void     malgtk_cell_renderer_score_get_property    (GObject                      *object,
+                                                            guint                         param_id,
+                                                            GValue                       *value,
+                                                            GParamSpec                   *pspec);
 
-static void     malgtk_cell_renderer_score_get_property  (GObject                    *object,
-                                                             guint                       param_id,
-                                                             GValue                     *value,
-                                                             GParamSpec                 *pspec);
+static void     malgtk_cell_renderer_score_set_property    (GObject                      *object,
+                                                            guint                         param_id,
+                                                            const GValue                 *value,
+                                                            GParamSpec                   *pspec);
 
-static void     malgtk_cell_renderer_score_set_property  (GObject                    *object,
-                                                             guint                       param_id,
-                                                             const GValue               *value,
-                                                             GParamSpec                 *pspec);
+static void     malgtk_cell_renderer_score_finalize        (GObject                      *gobject);
 
-static void     malgtk_cell_renderer_score_finalize (GObject *gobject);
-static void     malgtk_cell_renderer_score_dispose (GObject *gobject);
-static void     malgtk_cell_renderer_score_notify_score_cb(GObject *obj, GParamSpec *pspec, gpointer user_data);
+static void     malgtk_cell_renderer_score_dispose         (GObject                      *gobject);
+
+static void     malgtk_cell_renderer_score_notify_score_cb (GObject                      *obj,
+                                                            GParamSpec                   *pspec,
+                                                            gpointer                      user_data);
 
 enum
 {
   PROP_SCORE = 1,
 };
-
-G_DEFINE_TYPE (MalgtkCellRendererScore, malgtk_cell_renderer_score, GTK_TYPE_CELL_RENDERER_COMBO)
 
 enum {
     COLUMN_IDX,
@@ -34,17 +44,20 @@ enum {
     N_COLUMNS
 };
 
-static const char *malgtk_cell_renderer_score_array[] = { "\u2012", "1", "2", "3",
-                                              "4", "5", "6", "7", "8",
-                                              "9", "10" };
+static const char *malgtk_cell_renderer_score_array[] = {
+    "\u2012", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9", "10"
+};
+
 #define SCORE_ARRAY_SIZE (G_N_ELEMENTS(malgtk_cell_renderer_score_array))
 
 static void
-malgtk_cell_renderer_score_init (MalgtkCellRendererScore *cellrendererscore)
+malgtk_cell_renderer_score_init (MalgtkCellRendererScore *cellscore)
 {
-    cellrendererscore->score = 0;
-    cellrendererscore->liststore = gtk_list_store_new(N_COLUMNS, G_TYPE_INT, G_TYPE_STRING);
-    GtkListStore *store = cellrendererscore->liststore;
+    MalgtkCellRendererScorePrivate *priv = malgtk_cell_renderer_score_get_instance_private(cellscore);
+    priv->score = 0;
+    priv->liststore = gtk_list_store_new(N_COLUMNS, G_TYPE_INT, G_TYPE_STRING);
+    GtkListStore *store = priv->liststore;
 
     for (unsigned int i = 0; i < SCORE_ARRAY_SIZE; ++i) {
         gtk_list_store_insert_with_values(store, NULL, -1,
@@ -53,32 +66,33 @@ malgtk_cell_renderer_score_init (MalgtkCellRendererScore *cellrendererscore)
                                           -1);
     }
 
-    g_object_set(cellrendererscore,
+    g_object_set(cellscore,
                  "has-entry", FALSE,
                  "model", store,
                  "text-column", COLUMN_TXT,
                  "editable", TRUE,
                  NULL);
     
-    gtk_cell_renderer_set_alignment( GTK_CELL_RENDERER(cellrendererscore), 0.5, 0.5 );
-    g_signal_connect(cellrendererscore, "notify::score",
+    gtk_cell_renderer_set_alignment( GTK_CELL_RENDERER(cellscore), 0.5, 0.5 );
+    g_signal_connect(cellscore, "notify::score",
                      G_CALLBACK(malgtk_cell_renderer_score_notify_score_cb), store);
 }
 
 static void
 malgtk_cell_renderer_score_notify_score_cb(GObject *obj, GParamSpec *pspec, gpointer user_data)
 {
-    MalgtkCellRendererScore *cellrendererscore = MALGTK_CELL_RENDERER_SCORE(obj);
+    MalgtkCellRendererScore *cellscore = MALGTK_CELL_RENDERER_SCORE(obj);
     gint score;
 
-    g_object_get(cellrendererscore, "score", &score, NULL);
+    g_object_get(cellscore, "score", &score, NULL);
 
     if (((guint)score) < SCORE_ARRAY_SIZE) {
-        g_object_set(cellrendererscore, "text", malgtk_cell_renderer_score_array[(guint)score], NULL);
+        g_object_set(cellscore, "text", malgtk_cell_renderer_score_array[(guint)score], NULL);
     }
 }
 
-gint malgtk_cell_renderer_score_get_score_from_string(const gchar *str)
+gint
+malgtk_cell_renderer_score_get_score_from_string(const gchar *str)
 {
     if (g_ascii_isdigit(*str)) {
         guint64 idx = g_ascii_strtoull(str, NULL, 10);
@@ -117,8 +131,9 @@ malgtk_cell_renderer_score_class_init (MalgtkCellRendererScoreClass *klass)
 static void
 malgtk_cell_renderer_score_dispose (GObject *object)
 {
-    MalgtkCellRendererScore *cellrendererscore = MALGTK_CELL_RENDERER_SCORE(object);
-    g_clear_object(&cellrendererscore->liststore);
+    MalgtkCellRendererScore *cellscore = MALGTK_CELL_RENDERER_SCORE(object);
+    MalgtkCellRendererScorePrivate *priv = malgtk_cell_renderer_score_get_instance_private(cellscore);
+    g_clear_object(&priv->liststore);
 
     G_OBJECT_CLASS (malgtk_cell_renderer_score_parent_class)->dispose(object);
 }
@@ -136,11 +151,12 @@ malgtk_cell_renderer_score_get_property (GObject    *object,
                                             GParamSpec *psec)
 {
   MalgtkCellRendererScore  *cellscore = MALGTK_CELL_RENDERER_SCORE(object);
+  MalgtkCellRendererScorePrivate *priv = malgtk_cell_renderer_score_get_instance_private(cellscore);
 
   switch (param_id)
   {
     case PROP_SCORE:
-      g_value_set_int(value, cellscore->score);
+      g_value_set_int(value, priv->score);
       break;
 
     default:
@@ -156,11 +172,12 @@ malgtk_cell_renderer_score_set_property (GObject      *object,
                                             GParamSpec   *pspec)
 {
   MalgtkCellRendererScore *cellscore = MALGTK_CELL_RENDERER_SCORE (object);
+  MalgtkCellRendererScorePrivate *priv = malgtk_cell_renderer_score_get_instance_private(cellscore);
 
   switch (param_id)
   {
     case PROP_SCORE:
-      cellscore->score = g_value_get_int(value);
+      priv->score = g_value_get_int(value);
       break;
 
     default:
@@ -178,5 +195,6 @@ malgtk_cell_renderer_score_new (void)
 gint
 malgtk_cell_renderer_score_get_score(const MalgtkCellRendererScore* cellscore)
 {
-    return cellscore->score;
+    MalgtkCellRendererScorePrivate *priv = malgtk_cell_renderer_score_get_instance_private(cellscore);
+    return priv->score;
 }
--- src/gui/malgtk_cellrenderer_score.h
+++ src/gui/malgtk_cellrenderer_score.h
@@ -5,34 +5,12 @@
 G_BEGIN_DECLS
 
 #define MALGTK_TYPE_CELL_RENDERER_SCORE             (malgtk_cell_renderer_score_get_type ())
-#define MALGTK_CELL_RENDERER_SCORE(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj),  MALGTK_TYPE_CELL_RENDERER_SCORE, MalgtkCellRendererScore))
-#define MALGTK_CELL_RENDERER_SCORE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass),  MALGTK_TYPE_CELL_RENDERER_SCORE, MalgtkCellRendererScoreClass))
-#define MALGTK_IS_CELL_RENDERER_SCORE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MALGTK_TYPE_CELL_RENDERER_SCORE))
-#define MALGTK_IS_CELL_RENDERER_SCORE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass),  MALGTK_TYPE_CELL_RENDERER_SCORE))
-#define MALGTK_CELL_RENDERER_SCORE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj),  MALGTK_TYPE_CELL_RENDERER_SCORE, MalgtkCellRendererScoreClass))
-
-typedef struct _MalgtkCellRendererScore MalgtkCellRendererScore;
-typedef struct _MalgtkCellRendererScoreClass MalgtkCellRendererScoreClass;
-
-/* MalgtkCellRendererScore: Our malgtk cell renderer
- *   structure. Extend according to need */
-
-struct _MalgtkCellRendererScore
-{
-    GtkCellRendererCombo   parent;
-
-    gint           score;
-    GtkListStore *liststore;
-};
-
+G_DECLARE_DERIVABLE_TYPE(MalgtkCellRendererScore, malgtk_cell_renderer_score, MALGTK, CELL_RENDERER_SCORE, GtkCellRendererCombo)
 
 struct _MalgtkCellRendererScoreClass
 {
     GtkCellRendererComboClass  parent_class;
 };
-
-
-GType                malgtk_cell_renderer_score_get_type (void) G_GNUC_CONST;
 
 GtkCellRenderer     *malgtk_cell_renderer_score_new (void);
 
